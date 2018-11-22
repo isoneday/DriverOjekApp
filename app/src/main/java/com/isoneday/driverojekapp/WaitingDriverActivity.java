925bc1aac4a42d15b5a1e47aaf297d650c8c93a4
udakita <udakita11@gmail.com>
2018-11-22 09:42:00 +0700

    Initial commit

---
+++ app/src/main/java/com/isoneday/driverojekapp/WaitingDriverActivity.java
@@ -0,0 +1,145 @@
+package com.isoneday.driverojekapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.isoneday.driverojekapp.helper.HeroHelper;
+import com.isoneday.driverojekapp.helper.MyContants;
+import com.isoneday.driverojekapp.helper.SessionManager;
+import com.isoneday.driverojekapp.model.ResponseCheckBooking;
+import com.isoneday.driverojekapp.model.ResponseLoginRegis;
+import com.isoneday.driverojekapp.network.InitRetrofit;
+import com.isoneday.driverojekapp.network.RestApi;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import pl.bclogic.pulsator4droid.library.PulsatorLayout;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class WaitingDriverActivity extends AppCompatActivity {
+
+    @BindView(R.id.pulsator)
+    PulsatorLayout pulsator;
+    @BindView(R.id.buttoncancel)
+    Button buttoncancel;
+    private int idbooking;
+    private SessionManager session;
+    private String iddriver;
+    private Timer time;
+    private String token;
+    private String device;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_driver);
+        ButterKnife.bind(this);
+        pulsator.start();
+        idbooking = getIntent().getIntExtra(MyContants.IDBOOKING,0);
+        session = new SessionManager(this);
+        cekstatusbooking();
+        time = new Timer();
+    }
+
+    private void cekstatusbooking() {
+        RestApi api = InitRetrofit.getInstance();
+        Call<ResponseCheckBooking> bookingCall =api.checkbooking(idbooking);
+        bookingCall.enqueue(new Callback<ResponseCheckBooking>() {
+            @Override
+            public void onResponse(Call<ResponseCheckBooking> call, Response<ResponseCheckBooking> response) {
+                if (response.isSuccessful()){
+                    String result = response.body().getResult();
+                    String msg = response.body().getMsg();
+                    if (result.equals("true")){
+                        Toast.makeText(WaitingDriverActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(WaitingDriverActivity.this,DetailLokasiDriverActivity.class);
+                        iddriver = response.body().getDriver();
+                        i.putExtra(MyContants.IDDRIVER,iddriver);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        Toast.makeText(WaitingDriverActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseCheckBooking> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                cekstatusbooking();
+            }
+        },0,3000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        time.cancel();
+    }
+
+    @OnClick(R.id.buttoncancel)
+    public void onViewClicked() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cancel ");
+        builder.setMessage("Apakah anda yakin untuk cancel orderan ini ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               RestApi api = InitRetrofit.getInstance();
+               token =session.getToken();
+               device = HeroHelper.getDeviceUUID(WaitingDriverActivity.this);
+               Call<ResponseLoginRegis> regisCall = api.cancelbooking(idbooking,token,device);
+               regisCall.enqueue(new Callback<ResponseLoginRegis>() {
+                   @Override
+                   public void onResponse(Call<ResponseLoginRegis> call, Response<ResponseLoginRegis> response) {
+                       if (response.isSuccessful()){
+                           String result = response.body().getResult();
+                           String msg = response.body().getMsg();
+                    if (result.equals("true")){
+                        Toast.makeText(WaitingDriverActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else{
+                        Toast.makeText(WaitingDriverActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+                    }
+                       }
+                   }
+
+                   @Override
+                   public void onFailure(Call<ResponseLoginRegis> call, Throwable t) {
+
+                   }
+               });
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+
+    }
+}
---
+++ app/src/main/java/com/isoneday/driverojekapp/model/ResponseLoginRegis.java
@@ -0,0 +1,61 @@
+package com.isoneday.driverojekapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseLoginRegis{
+
+	@SerializedName("result")
+	private String result;
+
+	@SerializedName("msg")
+	private String msg;
+
+	@SerializedName("idUser")
+	private String idUser;
+
+	@SerializedName("data")
+	private DataUser data;
+
+	@SerializedName("token")
+	private String token;
+
+	public void setResult(String result){
+		this.result = result;
+	}
+
+	public String getResult(){
+		return result;
+	}
+
+	public void setMsg(String msg){
+		this.msg = msg;
+	}
+
+	public String getMsg(){
+		return msg;
+	}
+
+	public void setIdUser(String idUser){
+		this.idUser = idUser;
+	}
+
+	public String getIdUser(){
+		return idUser;
+	}
+
+	public void setData(DataUser data){
+		this.data = data;
+	}
+
+	public DataUser getData(){
+		return data;
+	}
+
+	public void setToken(String token){
+		this.token = token;
+	}
+
+	public String getToken(){
+		return token;
+	}
+}
